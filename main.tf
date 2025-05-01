0c1d79b5d5ce53d6fc75e9760dfb2477defc2d9e
VenkataPampana <103172677+VenkataPampana@users.noreply.github.com>
2025-05-01 12:20:08 +0100

    Update main.tf
    Updated main.tf and fixed the issues

--- main.tf
+++ main.tf
@@ -1,36 +1,37 @@
 resource "aws_instance" "web_host" {
-  ami           = "${var.ami}"
+  ami           = var.ami
   instance_type = "t2.nano"
 
-  vpc_security_group_ids = [
-  "${aws_security_group.web-node.id}"]
-  subnet_id = "${aws_subnet.web_subnet.id}"
+  vpc_security_group_ids = [aws_security_group.web-node.id]
+  subnet_id             = aws_subnet.web_subnet.id
+
   user_data = <<EOF
-#! /bin/bash
+#!/bin/bash
+set -e
 sudo apt-get update
 sudo apt-get install -y apache2
 sudo systemctl start apache2
 sudo systemctl enable apache2
-export AWS_DEFAULT_REGION=ap-southeast-2
 echo "<h1>Deployed via Terraform</h1>" | sudo tee /var/www/html/index.html
 EOF
+
   tags = {
     Name = "${local.resource_prefix.value}-ec2"
   }
 }
 
 resource "aws_ebs_volume" "web_host_storage" {
-  # unencrypted volume
-  availability_zone = "${var.availability_zone}"
-  #encrypted         = false  # Setting this causes the volume to be recreated on apply 
-  size = 1
+  availability_zone = var.availability_zone
+  size              = 1
+  encrypted         = true  # Added encryption for security
+
   tags = {
     Name = "${local.resource_prefix.value}-ebs"
   }
 }
 
 resource "aws_ebs_snapshot" "example_snapshot" {
-  volume_id   = "${aws_ebs_volume.web_host_storage.id}"
+  volume_id   = aws_ebs_volume.web_host_storage.id
   description = "${local.resource_prefix.value}-ebs-snapshot"
   tags = {
     Name = "${local.resource_prefix.value}-ebs-snapshot"
@@ -39,8 +40,8 @@ resource "aws_ebs_snapshot" "example_snapshot" {
 
 resource "aws_volume_attachment" "ebs_att" {
   device_name = "/dev/sdh"
-  volume_id   = "${aws_ebs_volume.web_host_storage.id}"
-  instance_id = "${aws_instance.web_host.id}"
+  volume_id   = aws_ebs_volume.web_host_storage.id
+  instance_id = aws_instance.web_host.id
 }
 
 resource "aws_security_group" "web-node" {
@@ -49,26 +50,26 @@ resource "aws_security_group" "web-node" {
   vpc_id      = aws_vpc.web_vpc.id
 
   ingress {
-    from_port = 80
-    to_port   = 80
-    protocol  = "tcp"
-    cidr_blocks = [
-    "0.0.0.0/0"]
+    from_port   = 80
+    to_port     = 80
+    protocol    = "tcp"
+    cidr_blocks = ["0.0.0.0/0"]  # Allow HTTP from anywhere
   }
+
   ingress {
-    from_port = 22
-    to_port   = 22
-    protocol  = "tcp"
-    cidr_blocks = [
-    "0.0.0.0/0"]
+    from_port   = 22
+    to_port     = 22
+    protocol    = "tcp"
+    cidr_blocks = ["YOUR.IP.ADDR.0/32"]  # Restrict SSH to your specific IP or CIDR range
   }
+
   egress {
-    from_port = 0
-    to_port   = 0
-    protocol  = "-1"
-    cidr_blocks = [
-    "0.0.0.0/0"]
+    from_port   = 0
+    to_port     = 0
+    protocol    = "-1"
+    cidr_blocks = ["0.0.0.0/0"]
   }
+
   depends_on = [aws_vpc.web_vpc]
 }
 
@@ -76,6 +77,7 @@ resource "aws_vpc" "web_vpc" {
   cidr_block           = "172.16.0.0/16"
   enable_dns_hostnames = true
   enable_dns_support   = true
+
   tags = {
     Name = "${local.resource_prefix.value}-vpc"
   }
@@ -102,7 +104,6 @@ resource "aws_subnet" "web_subnet2" {
     Name = "${local.resource_prefix.value}-subnet2"
   }
 }
-
 
 resource "aws_internet_gateway" "web_igw" {
   vpc_id = aws_vpc.web_vpc.id
@@ -140,10 +141,8 @@ resource "aws_route" "public_internet_gateway" {
   }
 }
 
-
 resource "aws_network_interface" "web-eni" {
-  subnet_id   = aws_subnet.web_subnet.id
-  private_ips = ["172.16.10.100"]
+  subnet_id = aws_subnet.web_subnet.id
 
   tags = {
     Name = "${local.resource_prefix.value}-primary_network_interface"
@@ -152,7 +151,7 @@ resource "aws_network_interface" "web-eni" {
 
 resource "aws_s3_bucket" "ctf-web-content" {
   bucket        = "${local.resource_prefix.value}-ctf-web-content"
-  acl           = "public-read-write"
+  acl           = "private"  # Set ACL to private for security
   force_destroy = true
 
   tags = {
